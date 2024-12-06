351046e24eb9a143e775d6e924fed12eef55600c
Ahmet Rauf Yavuz <ahmetraufyavuz@hotmail.com>
2024-12-06 20:05:19 +0300

    Bank Rest API
    Account Service done, will continue

---
+++ src/main/java/com/workintech/s18d4/entity/Account.java
@@ -0,0 +1,33 @@
+package com.workintech.s18d4.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "account", schema = "fsweb")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "money_amount")
+    private Double moneyAmount;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+
+
+
+}
